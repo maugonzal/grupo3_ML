04acc5085ac8f55e23dd5c6e98cf4b4cbfdaec4a
Mauricio Gonzalez <maugonzal@gmail.com>
2024-05-26 20:58:22 -0600

    Add files via upload

--- api_v15.py
+++ api_v15.py
@@ -8,11 +8,11 @@ from flask_restx import Api, Resource, fields
 from flask_cors import CORS
 
 def predict_proba(url):
-    reg = joblib.load(os.path.dirname(__file__) + '/model_xgb.pkl')
+    reg = joblib.load(os.path.dirname(__file__) + '/model.pkl')
 
-    module_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
+    module_url = "./tmp/"
     embed = hub.load(module_url)
-    x_embeddings = embed(url.tolist()).numpy()
+    x_embeddings = embed([url]).numpy()
 
     return reg.predict(x_embeddings)
 
